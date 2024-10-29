e4fa3661a30e64ffa014f421e6d59120bf1f88db
Samuel Van Der Hoeven <50185020+Dwalok@users.noreply.github.com>
2024-10-29 15:17:11 +0100

    Add files via upload

---
+++ static/templates/challenge7.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WeChallenge</title>
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+        <h1><a href="../../index.html">WeChallenge</a></h1>
+        <nav>
+            <a href="challenge1.php">Challenge 1</a>
+            <a href="challenge2.php">Challenge 2</a>
+            <a href="challenge3.php">Challenge 3</a>
+            <a href="challenge4.php">Challenge 4</a>
+            <a href="challenge5.php">Challenge 5</a>
+            <a href="challenge6.php">Challenge 6</a>
+            <a href="challenge7.php">Challenge 7</a>
+            <a href="challenge8.php">Challenge 8</a>
+        </nav>
+    </header>
+
+</html>
